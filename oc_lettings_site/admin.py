dd74c4ccc09ef53f32055e8c0d3e4fc7ab76d327
Quentin Hors <hors.quentin@outlook.fr>
2021-12-05 17:18:28 +0100

    Division de l'application oc_lettings_site en deux sous applications et modifications des fichiers pour leur utilisations

--- oc_lettings_site/admin.py
+++ oc_lettings_site/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from .models import Letting
-from .models import Address
-from .models import Profile
-
+from lettings.models import Address
+from lettings.models import Letting
+from profiles.models import Profile
 
 admin.site.register(Letting)
 admin.site.register(Address)
