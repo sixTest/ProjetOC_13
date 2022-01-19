3cdc4e25a519e749ec4909c43d4cd3aee895a43b
Quentin Hors <hors.quentin@outlook.fr>
2022-01-19 17:07:50 +0100

    test demo sentry et heroku

--- projet13/urls.py
+++ projet13/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    print(division_by_zero)
+
+
 urlpatterns = [
     path('', include('oc_lettings_site.urls', namespace='oc_lettings_site')),
     path('lettings/', include('lettings.urls', namespace='lettings')),
     path('profiles/', include('profiles.urls', namespace='profiles')),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
